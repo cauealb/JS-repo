642025822308a42320427b345f76b2221fa23041
Cauê Alves <cauealvesb4@gmail.com>
2025-05-02 21:47:29 -0300

    Alterando o comportamento do html

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,11 +1,11 @@
 // Exibindo com textContent
-// const guest = document.querySelector("#guest-1")
-// console.log(guest.textContent)
+const guest = document.querySelector("#guest-1")
+console.log(guest.textContent)
 
 // Exibindo com innerText
-// const guest = document.querySelector("#guest-1")
-// console.log(guest.innerText)
+const guest1 = document.querySelector("#guest-1")
+console.log(guest1.innerText)
 
 // Exibindo com innerHTML
-// const guest = document.querySelector("#guest-1")
-// console.log(guest.innerHTML)
+const guest2 = document.querySelector("#guest-1")
+console.log(guest2.innerHTML)
