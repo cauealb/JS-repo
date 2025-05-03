82ad38e731bf224e63d7685d2cef187495743b7a
Cauê Alves <cauealvesb4@gmail.com>
2025-05-02 21:44:18 -0300

    Exibindo com innerText

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,3 +1,7 @@
 // Exibindo com textContent
+// const guest = document.querySelector("#guest-1")
+// console.log(guest.textContent)
+
+// Exibindo com innerText
 const guest = document.querySelector("#guest-1")
-console.log(guest.textContent)
+console.log(guest.innerText)
