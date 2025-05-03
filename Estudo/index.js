f274572b5e63772d276c69c6f8fb9d947b706cf0
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 18:16:02 -0300

    Adicionando um evento

--- Estudo/index.js
+++ Estudo/index.js
@@ -5,12 +5,19 @@ window.addEventListener("load", (e) => {
 
 // Colocando o evento no documento
 addEventListener("click", (e) => {
-    // Deixando o evento como padrão
+//     // Deixando o evento como padrão
     e.preventDefault()
 
-    // Pegando o evento o elemento que foi clicado
-    console.log(e.target)
+//     // Pegando o evento o elemento que foi clicado
+//     console.log(e.target)
 
-    // Pegando o texto que tem ao clicar no elemento
+//     // Pegando o texto que tem ao clicar no elemento
+//     console.log(e.target.textContent)
+})
+
+
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", (e) => {
     console.log(e.target.textContent)
 })
