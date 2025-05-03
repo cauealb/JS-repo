efc727e4532181157b1073d1664eb7a1d55a0d12
Cauê Alves <cauealvesb4@gmail.com>
2025-05-02 21:12:00 -0300

    Acessando elementos pelo querySelectorAll

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,3 +1,7 @@
 // Acessando elementos pelo querySelector
-const guest_1 = document.querySelector("#guest-1")
-console.log(guest_1)
+// const guest_1 = document.querySelector("#guest-1")
+// console.log(guest_1)
+
+// Acessando elementos pelo querySelectorAll
+const guests = document.querySelectorAll(".guest")
+console.log(guests)
