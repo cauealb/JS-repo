892507d39ba3e216052db97c29902cb416dcf444
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 16:55:15 -0300

    Usando a função toggle para verificar se tem a classe, se tiver ele remove, se não ele adiciona

--- Estudo/index.js
+++ Estudo/index.js
@@ -4,4 +4,7 @@ const input1 = document.querySelector("#task")
 input1.classList.add("input-error")
 
 // Removendo classes já existentes em um elemento
-input1.classList.remove("input-error")
+// input1.classList.remove("input-error")
+
+// Usando a função toggle para verificar se tem a classe, se tiver ele remove, se não ele adiciona
+input1.classList.toggle("input-error")
