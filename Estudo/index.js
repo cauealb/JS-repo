bd49f2dc2f6885093a7798c9b2aa473d8caea288
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 19:11:04 -0300

    Incluindo mais li no html e estilizando o css da ul

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,23 +1,2 @@
-// Colocando evento da tela toda
-window.addEventListener("load", (e) => {
-    console.log("Tela foi recarregada com sucesso!")
-})
+// Adicionando um evento de scroll
 
-// Colocando o evento no documento
-addEventListener("click", (e) => {
-//     // Deixando o evento como padrão
-    e.preventDefault()
-
-//     // Pegando o evento o elemento que foi clicado
-//     console.log(e.target)
-
-//     // Pegando o texto que tem ao clicar no elemento
-//     console.log(e.target.textContent)
-})
-
-
-const btn = document.querySelector("#btn");
-
-btn.addEventListener("click", (e) => {
-    console.log(e.target.textContent)
-})
