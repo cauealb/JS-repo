7c52558f5efc85844df4f4f2ec6e7d052e2ee195
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 17:31:23 -0300

    Adicionando classes em elementos que criamos

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,6 +1,7 @@
 const listTask = document.querySelector("ul");
 
 const newTask = document.createElement("li");
+newTask.classList.add("guest")
 const newTaksName = document.createElement("span");
 
 newTaksName.textContent = "Cauê";
