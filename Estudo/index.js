5dd917005bacfb385a82e5093703d5bb5f243792
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 20:01:54 -0300

    Adicionando um evento sem ser o addEventListener

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,11 +1,7 @@
-// Adicionando um evento de scroll
-const ul = document.querySelector("ul");
-ul.addEventListener("scroll", () => {
-    // Adicionando verificação da onde o scroll está em relação ao top
-    if(ul.scrollTop > 206) {
-        ul.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        })
-    }
-})
+// Adicionando um evento sem ser o addEventListener
+const form = document.querySelector("form");
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+    console.log("Você enviou algo no formulário!!");
+}
