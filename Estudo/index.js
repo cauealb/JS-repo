e5115c17e0346453a7781b4fef42fb3499e881cd
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 20:05:25 -0300

    Diferença entre on... e addEventListener

--- Estudo/index.js
+++ Estudo/index.js
@@ -3,5 +3,20 @@ const form = document.querySelector("form");
 
 form.onsubmit = (e) => {
     e.preventDefault()
-    console.log("Você enviou algo no formulário!!");
+    console.log("Você enviou algo no formulário!! #1");
 }
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+    console.log("Você enviou algo no formulário!! #2");
+}
+
+form.addEventListener("submit" , (e) => {
+    e.preventDefault()
+    console.log("Você enviou algo no formulário!! #3");
+})
+
+form.addEventListener("submit" , (e) => {
+    e.preventDefault()
+    console.log("Você enviou algo no formulário!! #4");
+})
