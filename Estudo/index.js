549bcedbd57503738f1ae802d13250fa59733b63
Cauê Alves <cauealvesb4@gmail.com>
2025-05-02 21:45:13 -0300

    Exibindo com innerHTML

--- Estudo/index.js
+++ Estudo/index.js
@@ -3,5 +3,9 @@
 // console.log(guest.textContent)
 
 // Exibindo com innerText
-const guest = document.querySelector("#guest-1")
-console.log(guest.innerText)
+// const guest = document.querySelector("#guest-1")
+// console.log(guest.innerText)
+
+// Exibindo com innerHTML
+// const guest = document.querySelector("#guest-1")
+// console.log(guest.innerHTML)
