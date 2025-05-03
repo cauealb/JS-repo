bf05bf74e4c583a850daff4573d796d4b7020612
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 17:28:17 -0300

    Método para colocar um elemento dentro de outro elemento

--- Estudo/index.js
+++ Estudo/index.js
@@ -3,3 +3,7 @@ const listTask = document.getElementsByTagName("ul");
 const newTask = document.createElement("li");
 const newTaksName = document.createElement("span");
 
+newTaksName.textContent = "Cauê";
+newTask.append(newTaksName);
+
+console.log(newTask);
