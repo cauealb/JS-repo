1eebe24bb9aa19590272ce73f8c8cbd3a9b1f835
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 16:56:59 -0300

    Adicionado estilos no JS

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,10 +1,16 @@
 // Adicionando classes já existentes em um elemento
-const input1 = document.querySelector("#task")
+// const input1 = document.querySelector("#task")
 
-input1.classList.add("input-error")
+// input1.classList.add("input-error")
 
 // Removendo classes já existentes em um elemento
 // input1.classList.remove("input-error")
 
 // Usando a função toggle para verificar se tem a classe, se tiver ele remove, se não ele adiciona
-input1.classList.toggle("input-error")
+// input1.classList.toggle("input-error")
+
+// Adicionado estilos no JS
+const btn = document.querySelector("button")
+
+btn.style.color = "Black"
+btn.style.backgroundColor = "White"
