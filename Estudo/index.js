6e8f659eeb520df35d06b75b9c29363e6a13bca5
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 19:16:05 -0300

    Adicionando verificação da onde o scroll está em relação ao top

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,6 +1,11 @@
 // Adicionando um evento de scroll
 const ul = document.querySelector("ul");
 ul.addEventListener("scroll", () => {
-    
-    
+    // Adicionando verificação da onde o scroll está em relação ao top
+    if(ul.scrollTop > 206) {
+        ul.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
 })
