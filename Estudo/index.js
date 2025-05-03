4a956e05a1c78991de06a5680900fe91810c779f
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 18:09:00 -0300

    Pegando o evento o elemento que foi clicado

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,5 +1,12 @@
 // Colocando evento da tela toda
-
 window.addEventListener("load", (e) => {
     console.log("Tela foi recarregada com sucesso!")
+})
+
+// Colocando o evento no documento
+addEventListener("click", (e) => {
+    // console.log(e)
+
+    // Pegando o evento o elemento que foi clicado
+    console.log(e.target)
 })
