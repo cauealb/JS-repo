e9d9ae05043dfa2321268b709f542d997f79017a
Cauê Alves <cauealvesb4@gmail.com>
2025-05-03 17:47:57 -0300

    Removendo atributuos

--- Estudo/index.js
+++ Estudo/index.js
@@ -2,3 +2,7 @@ const btn = document.getElementById("btn");
 
 // Adicioando ou atualizando um atributo
 btn.setAttribute("disabled", true)
+
+// Removendo atributuos
+btn.removeAttribute("id")
+btn.removeAttribute("disabled")
