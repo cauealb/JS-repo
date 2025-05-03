38b3ad56a752450124a14b4f61761541d72ff248
Cauê Alves <cauealvesb4@gmail.com>
2025-05-02 21:10:29 -0300

    Acessando elementos pelo querySelector

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,27 +1,3 @@
-// Venda a Estrutura do documento HTML
-// console.log(document)
-
-// Selecionando o título da página
-// console.log(document.title)
-
-// Selecionando pelo Id
-// const guest1 = document.getElementById("guest-2")
-// console.log(guest1)
-
-// Selecionando o elemento pela Classe
-// const guestElements = document.getElementsByClassName("guest")
-// console.log(guestElements)
-
-// Selecionando um dos elementos do array HTML collection
-// console.log(guestElements.item(0))
-// console.log(guestElements[1])
-
-// Selecionando elementos pela tag
-// const arrTag = document.getElementsByTagName("li")
-// console.log(arrTag.item(0))
-
-// Vendo as propríedades
-// const heroClass = document.getElementsByClassName("hero")
-// console.dir(heroClass)
-
-document.title = "Python"
+// Acessando elementos pelo querySelector
+const guest_1 = document.querySelector("#guest-1")
+console.log(guest_1)
