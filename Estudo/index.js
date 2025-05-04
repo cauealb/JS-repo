fc52f5bc91dbecfe2d5efb17b59043c21321b3e3
Cauê Alves <cauealvesb4@gmail.com>
2025-05-04 10:31:15 -0300

    Aprendendo o evento de KeyPress

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,5 +1,11 @@
 // Aprendendo o evento de KeyDown
-const input = document.querySelector("input");
-input.addEventListener("keydown", (e) => {
-    console.log(e.key)
+// const input = document.querySelector("input");
+// input.addEventListener("keydown", (e) => {
+//     console.log(e.key)
+// })
+
+// Aprendendo o evento de KeyPress
+const input = document.querySelector("input")
+input.addEventListener("keypress", (e) => {
+    console.log(e.target.textContent)
 })
