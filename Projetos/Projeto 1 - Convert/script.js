35e015d4fd5e86bdfc037e0a666d5ea978fbeee5
Cauê Alves <cauealvesb4@gmail.com>
2025-05-04 19:43:26 -0300

    Executando o evento quando ele dá um submit no formulário

--- Projetos/Projeto 1 - Convert/script.js
+++ Projetos/Projeto 1 - Convert/script.js
@@ -1,3 +1,8 @@
+// Cotação das moedas
+const USD = 5.66
+const EUR = 6.40
+const GBP = 7.50
+
 // Pegando o valor do input
 const form = document.querySelector("form");
 const amounInput = document.getElementById("amount");
@@ -8,3 +13,12 @@ amounInput.addEventListener("input", () => {
     const regex = /\D+/g;
     amounInput.value = amounInput.value.replace(regex, "")
 })
+
+// Executando o evento quando ele dá um submit no formulário
+form.onsubmit = (e) => {
+    e.preventDefault();
+}
+
+const convertCurrency = function(currency, price, sybol) {
+    console.log(currency, price, sybol)
+}
