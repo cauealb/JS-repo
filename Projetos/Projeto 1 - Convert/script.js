bab65b74ef35861081c2f45a2a74dde2c869d89f
Cauê Alves <cauealvesb4@gmail.com>
2025-05-04 19:07:15 -0300

    Validando input

--- Projetos/Projeto 1 - Convert/script.js
+++ Projetos/Projeto 1 - Convert/script.js
@@ -1,5 +1,7 @@
 // Pegando o valor do input
 const amounInput = document.getElementById("amount");
 amounInput.addEventListener("input", () => {
-    
+    // Validando input
+    const regex = /\D+/g;
+    amounInput.value = amounInput.value.replace(regex, "")
 })
