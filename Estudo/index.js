573d07cb299386f0f21d425371535ed35dc64e07
Cauê Alves <cauealvesb4@gmail.com>
2025-05-04 10:28:26 -0300

    Aprendendo o evento de KeyDown

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,22 +1,5 @@
-// Adicionando um evento sem ser o addEventListener
-const form = document.querySelector("form");
-
-form.onsubmit = (e) => {
-    e.preventDefault()
-    console.log("Você enviou algo no formulário!! #1");
-}
-
-form.onsubmit = (e) => {
-    e.preventDefault()
-    console.log("Você enviou algo no formulário!! #2");
-}
-
-form.addEventListener("submit" , (e) => {
-    e.preventDefault()
-    console.log("Você enviou algo no formulário!! #3");
-})
-
-form.addEventListener("submit" , (e) => {
-    e.preventDefault()
-    console.log("Você enviou algo no formulário!! #4");
+// Aprendendo o evento de KeyDown
+const input = document.querySelector("input");
+input.addEventListener("keydown", (e) => {
+    console.log(e.key)
 })
