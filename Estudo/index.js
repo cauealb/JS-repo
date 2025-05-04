e09c143767fb99c0c890e8520b81971d702d0d6c
Cauê Alves <cauealvesb4@gmail.com>
2025-05-04 12:10:52 -0300

    Criando um evento de submit para o form

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,12 +1,20 @@
 // Novo evento de input
 const input = document.querySelector("input")
-input.addEventListener("input", () => {
-    // Expressões Regulares
-    const value = input.value
-    const regex = /\D+/g
+// input.addEventListener("input", () => {
+//     // Expressões Regulares
+//     const value = input.value
+//     const regex = /\D+/g
 
-    // Match com regex
-    // console.log(value.match(regex))
-    // Usando test com regex
-    console.log(regex.test(value))
+//     // Match com regex
+//     // console.log(value.match(regex))
+//     // Usando test com regex
+//     // console.log(regex.test(value))
+// })
+
+// Criando um evento de submit para o form
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    console.log("Oi")
 })
