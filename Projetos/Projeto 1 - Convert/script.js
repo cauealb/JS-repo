6e447ad7a040c2bfd258b603090c37acc32351d0
Cauê Alves <cauealvesb4@gmail.com>
2025-05-04 19:15:34 -0300

    Pegando o valor do input

--- Projetos/Projeto 1 - Convert/script.js
+++ Projetos/Projeto 1 - Convert/script.js
@@ -1,5 +1,8 @@
 // Pegando o valor do input
+const form = document.querySelector("form");
 const amounInput = document.getElementById("amount");
+const currency = document.getElementById("currency");
+
 amounInput.addEventListener("input", () => {
     // Validando input
     const regex = /\D+/g;
