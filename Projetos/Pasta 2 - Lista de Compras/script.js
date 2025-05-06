d64e8a4683c7e0e1f49d6d05c440126bafdecb66
Cauê Alves <cauealvesb4@gmail.com>
2025-05-06 20:43:09 -0300

    feat: Fazendo validação do campo do input

--- Projetos/Pasta 2 - Lista de Compras/script.js
+++ Projetos/Pasta 2 - Lista de Compras/script.js
@@ -1,1 +1,13 @@
-console.log("Olá Vannila")
+const form = document.querySelector("#input");
+const input = document.querySelector("#namePurchases");
+
+// Eventos
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if(input.value === "") {
+        alert("Digite sua compra!");
+        return
+    }
+
+})
