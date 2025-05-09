b36959705c467bdc4de8c1dccbbada96616f2d43
Cauê Alves <cauealvesb4@gmail.com>
2025-05-09 00:08:17 -0300

    test: Criando estrutura das perguntas

--- Projetos/Projeto 3 - Quiz/script.js
+++ Projetos/Projeto 3 - Quiz/script.js
@@ -5,6 +5,29 @@ const placeButton = document.getElementById("options-container");
 // Variável que controla as perguntas
 const currentQuetions = 0
 
+// Estrutura das perguntas
+
+const Quiz = [
+    {
+        id: 1,
+        question: "Messi?",
+        options: ["Não", "Sim", "Talvez"],
+        answer: 1
+    },
+    {
+        id: 2,
+        question: "Cristiano Ronaldo?",
+        options: ["Não", "Sim", "Talvez"],
+        answer: 0
+    },
+    {
+        id: 3,
+        question: "Neymar?",
+        options: ["Não", "Sim", "Talvez"],
+        answer: 2
+    }
+]
+
 document.addEventListener("click", () => {
     currentQuetions++
 })
