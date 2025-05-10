d9431c59d2486af5976160cc761d379fb0abd8ae
Cauê Alves <cauealvesb4@gmail.com>
2025-05-10 13:36:58 -0300

    feat: Fazendo validações básicas

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,3 +1,22 @@
 const nameIput = document.getElementById("name");
-const ageeInput = document.getElementById("age");
+const ageInput = document.getElementById("age");
 const professioninput = document.getElementById("profession");
+const btnJSON = document.getElementById("btnJSON");
+
+btnJSON.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(nameIput.value === "") {
+        alert("Coloque seu nome!");
+        nameIput.focus();
+
+    } else if(ageInput.value === "") {
+        alert("Coloque sua idade!");
+        ageInput.focus();
+        
+    } else if (professioninput.value === "") {
+        alert("Coloque sua profissão!");
+        professioninput.focus()
+    }
+})
+
