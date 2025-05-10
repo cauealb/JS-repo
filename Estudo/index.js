be348885ea0da75b4523fa065a3aa961d172face
Cauê Alves <cauealvesb4@gmail.com>
2025-05-10 12:50:10 -0300

    Começando os estudos de LocalStorage

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,29 +1,8 @@
-// Novo evento de input
-const input = document.querySelector("input")
-// input.addEventListener("input", () => {
-//     // Expressões Regulares
-//     const value = input.value
-//     const regex = /\D+/g
+const pessoa = {
+    name: "Cauê",
+    age: 19,
+}
 
-//     // Match com regex
-//     // console.log(value.match(regex))
-//     // Usando test com regex
-//     // console.log(regex.test(value))
-// })
+localStorage.setItem("json", JSON.stringify(pessoa))
 
-// Criando um evento de submit para o form
-const form = document.querySelector("form")
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    // Usando replace com Regex
-    const value = input.value;
-    const regex = /\D+/g;
-    // console.log(value.replace(regex, ""))
-
-    if(regex.test(value)) {
-        console.log(value)
-    } else {
-        alert("Valor Inválido")
-    }
-})
+console.log(JSON.stringify(pessoa))
