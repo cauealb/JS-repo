99591ac3c902d7c6d96b49ee7fc52de8c1caa66c
Cauê Alves <cauealvesb4@gmail.com>
2025-05-11 17:40:41 -0300

    Criando uma outra função construtora de uma forma diferente

--- Estudo/index.js
+++ Estudo/index.js
@@ -19,3 +19,15 @@ const person2 = new Person("Geovanna", 1.60)
 console.log(person2.name)
 
 console.log(person1 === person2)
+
+// Criando uma outra função construtora de uma forma diferente
+function Product(name, price) {
+    this.name = name;
+    this.heigth = price;
+
+    const produto = {};
+    produto.name = this.name;
+    produto.price = this.price;
+
+    
+}
