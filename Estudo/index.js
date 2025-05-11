d2a0ebf94e1645d279f190d3af5132eacee31338
Cauê Alves <cauealvesb4@gmail.com>
2025-05-11 15:25:35 -0300

    Criando um objeto com propriedades e métodos

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,3 +1,21 @@
 // Criando um objeto vázio
 const obj = {};
 console.log(typeof obj)
+
+// Criando um objeto com propriedades e métodos
+const user = {
+    name: {
+        firt_name: "Cauê",
+        last_name: "Alves",
+    },
+    age: 19,
+    adrees: {
+        city: "São Paulo",
+        number: 33,
+        street: "Rua XYZ"
+    },
+    stacks: ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Python", "Visual Basics"],
+    girl_friend: "Geovanna Alves",
+    role: "Software Enginner",
+    experience: "7 months"
+}
