d62000e4cd35e9d2c1f18ac0bbac25d0127661c4
Cauê Alves <cauealvesb4@gmail.com>
2025-05-11 16:48:02 -0300

    Acessando com Opitional Chaning

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,6 +1,9 @@
 // Criando um objeto teste
 const product = {
     name: "Teclado",
-    price: 127,
+    // price: 127,
     shopping: ["União", "Tamboré", "Plazza"]
 }
+
+// Acessando com Opitional Chaning
+console.log(product?.price)
