83952499bcb74a2c99a1b492febbda572f0c1d36
Cauê Alves <cauealvesb4@gmail.com>
2025-05-11 17:36:13 -0300

    Instâncioando a função contrutura

--- Estudo/index.js
+++ Estudo/index.js
@@ -4,9 +4,13 @@ function Person(name, heigth) {
 
     personOBJ.name = name;
     personOBJ.heigth = heigth;
-    personOBJ.message = () => {
+    personOBJ.message = function() {
         console.log(`Esse é ${this.name} e tem ${this.heigth}`)
     }
 
     return personOBJ;
 }
+
+// Instâncioando a função contrutura
+const person1 = new Person("Cauê", 1.67)
+person1.message();
