b30ed8892c0a44a753e208cdac2bdadc98bf3574
Cauê Alves <cauealvesb4@gmail.com>
2025-05-11 18:05:54 -0300

    Resolvendo exercício de função construtor

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,34 +1,15 @@
-// Criando uma função construtora
-function Person(name, heigth) {
-    const personOBJ = {};
-
-    personOBJ.name = name;
-    personOBJ.heigth = heigth;
-    personOBJ.message = function() {
-        console.log(`Esse é ${this.name} e tem ${this.heigth}`)
-    }
-
-    return personOBJ;
-}
-
-// Instâncioando a função contrutura
-const person1 = new Person("Cauê", 1.67)
-person1.message();
-
-const person2 = new Person("Geovanna", 1.60)
-console.log(person2.name)
-
-console.log(person1 === person2)
-
-// Criando uma outra função construtora de uma forma diferente
-function Product(name, price) {
+function Livro(name, author, age) {
     this.name = name;
-    this.heigth = price;
+    this.author = author
+    this.age = age
+    this.description = function() {
+        console.log(`O livro ${this.name} foi escrito por ${this.author} em ${this.age}`)
+    }
 }
 
-const caue = new Product("teclado", 130)
-console.log(caue.name)
+const book1 = new Livro("1984", "George Orwell", 1999)
+const book2 = new Livro("Correr ou Morrer", "Adam White", 2007)
+const book3 = new Livro("Jogos Vorazes", "Natasha Between", 1887)
 
-
-const teste = new String("Cauê")
-console.log(teste[3])
+console.log(book1)
+book1.description()
