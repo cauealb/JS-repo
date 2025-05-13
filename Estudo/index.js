9fababdba580699d6fe4e3dcf0f7488c16101df8
Cauê Alves <cauealvesb4@gmail.com>
2025-05-12 22:52:57 -0300

    Pegando uma fatia de uma string

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,3 +1,7 @@
 // Imprimindo o comprimento da string
 const creditCard = "1234567891234567"
-console.log(creditCard.length)
+// console.log(creditCard.length)
+
+// Pegando uma fatia de uma string
+const lastDigits = creditCard.slice(-4)
+console.log(lastDigits)
