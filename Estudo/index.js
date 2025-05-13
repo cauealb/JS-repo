d1049319c82226998e368e18fd05038c8708b2ce
Cauê Alves <cauealvesb4@gmail.com>
2025-05-12 23:14:26 -0300

    juntando uma string com join

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,3 +1,9 @@
 // Separando um array
 const lbl = "Estou aprendendo Javascript";
-console.log(lbl.split(""))
+const separete = lbl.split("")
+console.log(separete)
+
+
+// juntando uma string com join
+const newArr = separete.join("")
+console.log(newArr)
