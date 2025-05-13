498797206e44b3564cb7a5b889cb4409f1d9810a
Cauê Alves <cauealvesb4@gmail.com>
2025-05-12 23:10:36 -0300

    Separando um array com split

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,16 +1,3 @@
-// Imprimindo o comprimento da string
-const creditCard = "1234567891234567"
-// console.log(creditCard.length)
-
-// Pegando uma fatia de uma string
-const lastDigits = creditCard.slice(-4)
-// console.log(lastDigits)
-
-// Usando padStart para colocar conteúdo no inicío da string
-const newCreditCard = lastDigits.padStart(creditCard.length, "X")
-console.log(newCreditCard)
-
-
-// Usando padEnd para colocar conteúdo no final da string
-const test = lastDigits.padEnd(10, "$")
-console.log(test)
+// Separando um array
+const lbl = "Estou aprendendo Javascript";
+console.log(lbl.split(""))
