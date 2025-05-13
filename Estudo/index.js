0c3f23fae03eabfa6baf17837acd38375be97c74
Cauê Alves <cauealvesb4@gmail.com>
2025-05-12 23:57:06 -0300

    Usando o includes

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,3 +1,7 @@
 // Usando o método indexOf
 const message = "Estou estudando a BASE do Javascript"
 console.log(message.indexOf("BASE"))
+
+
+// Usando o includes
+console.log(message.includes("BASA"))
