ec74c71eb976953f3d75de08b528f7924a44c0f4
Cauê Alves <cauealvesb4@gmail.com>
2025-05-13 01:06:13 -0300

    Utilizando a função splice

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,4 +1,7 @@
 // Achando um index de um Array
 const arrFruits = ["Banana", "Lemon", "Orange"]
-
 console.log(arrFruits.indexOf("Orange"))
+
+// Utilizando a função splice
+arrFruits.splice(1, 2)
+console.log(arrFruits)
