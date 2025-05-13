73ef7cbbc250833d246e8edc1a0bcdd3a92ad59c
Cauê Alves <cauealvesb4@gmail.com>
2025-05-12 22:51:40 -0300

    Imprimindo o comprimento da string

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,15 +1,3 @@
-function Livro(name, author, age) {
-    this.name = name;
-    this.author = author
-    this.age = age
-    this.description = function() {
-        console.log(`O livro ${this.name} foi escrito por ${this.author} em ${this.age}`)
-    }
-}
-
-const book1 = new Livro("1984", "George Orwell", 1999)
-const book2 = new Livro("Correr ou Morrer", "Adam White", 2007)
-const book3 = new Livro("Jogos Vorazes", "Natasha Between", 1887)
-
-console.log(book1)
-book1.description()
+// Imprimindo o comprimento da string
+const creditCard = "1234567891234567"
+console.log(creditCard.length)
