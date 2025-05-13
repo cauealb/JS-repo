d1a463ecbb9ab126fbe85bbedee078e0c8f4086b
Cauê Alves <cauealvesb4@gmail.com>
2025-05-13 01:09:26 -0300

    Fazendo validação para ver se tem no array

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,6 +1,11 @@
 // Achando um index de um Array
 const arrFruits = ["Banana", "Lemon", "Orange"]
-console.log(arrFruits.indexOf("Orange"))
+
+if(arrFruits.includes("Lemon")) {
+    console.log(arrFruits.indexOf("Lemon"))
+} else {
+    console.log("Não exitem dados!")
+}
 
 // Utilizando a função splice
 arrFruits.splice(1, 2)
