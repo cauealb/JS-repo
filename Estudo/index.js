f9c13b6fe394504c2164824c6970dcdf25670d41
Cauê Alves <cauealvesb4@gmail.com>
2025-05-14 19:33:15 -0300

    Convertendo para string só o tempo

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,4 +1,7 @@
-const date = new Date(2025, 2, 20);
+const date = new Date(2025, 2, 20, 12, 56, 7);
 
 // Convertendo para string só com a data
-console.log(date.toDateString())
+console.log(date.toDateString());
+
+// Convertendo para string só o tempo
+console.log(date.toTimeString());
