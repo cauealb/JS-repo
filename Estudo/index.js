8b04c0caf7ab61cbb6416bfa7af2cae65503432f
Cauê Alves <cauealvesb4@gmail.com>
2025-05-14 20:44:42 -0300

    Exibindo dats com intl

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,2 +1,6 @@
 // Obtém dados da localidade
 console.log(new Intl.DateTimeFormat().resolvedOptions())
+
+// Exibindo dats com intl
+console.log(new Intl.DateTimeFormat("pt-BR").format(new Date))
+console.log(new Intl.DateTimeFormat("en-US").format(new Date))
