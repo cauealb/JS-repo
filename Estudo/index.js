870b889da2b79f12ed99b9a29daab24fe7d43a72
Cauê Alves <cauealvesb4@gmail.com>
2025-05-14 18:05:05 -0300

    Setar o dia

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,5 +1,10 @@
-// Setando o ano
 const newDate = new Date("July 4, 2025, 09:55:59");
 
+// Setando o ano
 newDate.setFullYear("2020")
+
+// Setar o dia
+newDate.setDate(20)
+
+
 console.log(newDate)
