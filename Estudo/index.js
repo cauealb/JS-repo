9d3d21f40f3dafd1aeba5eb15acfe099ddc42fc1
Cauê Alves <cauealvesb4@gmail.com>
2025-05-14 18:03:25 -0300

    Setando o ano

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,25 +1,5 @@
-const date = new Date("2025-02-20");
+// Setando o ano
+const newDate = new Date("July 4, 2025, 09:55:59");
 
-// Pega o dia da semana
-console.log(date.getDay())
-
-// Pega o dia do mês
-console.log(date.getDate())
-
-// Pega o mês
-console.log(date.getMonth())
-
-// Pega o ano
-console.log(date.getFullYear())
-
-// Pega as horas
-console.log(date.getHours())
-
-// Pega os minutos
-console.log(date.getMinutes())
-
-// Pega os segundos
-console.log(date.getSeconds())
-
-// Pega os milisegundos
-console.log(date.getMilliseconds())
+newDate.setFullYear("2020")
+console.log(newDate)
