d93b64d83b0ec7eb0a8d6791ddf57d8e6ec2def0
Cauê Alves <cauealvesb4@gmail.com>
2025-05-13 23:53:48 -0300

    Pega o dia da semana

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,11 +1,4 @@
-// Exibindo a data do jeito que eu quero
-console.log(new Date(2025, 2, 20))
+const date = new Date("2025-02-20");
 
-// Colocando a data com outro argumento
-console.log(new Date(2024, 2, 20, 23, 39, 0))
-
-// Colocando a data com string
-console.log(new Date("2024-03-20T12:12:12"));
-
-// Colocando o nome do mês
-console.log(new Date("March 20, 2025 13:13:55"))
+// Pega o dia da semana
+console.log(date.getDay())
