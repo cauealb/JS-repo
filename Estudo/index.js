9e3a56a355e2d437cc5b05ef1521314d6f817d76
Cauê Alves <cauealvesb4@gmail.com>
2025-05-14 20:42:47 -0300

    Obtém dados da localidade

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,27 +1,2 @@
-const date = new Date(2025, 2, 20, 12, 12, 12);
-
-// Exibindo data como  short no toLocaleString
-console.log(date.toLocaleString("pt-BR", {
-    dateStyle: "short"
-}))
-
-// Exibindo data como long no toLocaleString
-console.log(date.toLocaleString("pt-BR", {
-    dateStyle: 'full'
-}))
-
-// Exibindo dados que eu quero no toLocaleString
-console.log(date.toLocaleString("pt-BR", {
-    day: '2-digit',
-    month: '2-digit',
-    minute: '2-digit',
-    year: "2-digit"
-}))
-
-// Usando o toLocaleString com variáveis qualquer
-const moeda = 126.34
-console.log(moeda.toLocaleString("pt-BR", {
-    style: "currency",
-    // Colocando como moeda brasileira
-    currency: "BRL",
-}))
+// Obtém dados da localidade
+console.log(new Intl.DateTimeFormat().resolvedOptions())
