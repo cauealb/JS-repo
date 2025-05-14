e3237072186aad85d8ab553075938a2ebff3122b
Cauê Alves <cauealvesb4@gmail.com>
2025-05-14 19:42:19 -0300

    Exibindo a hora em formate da minha região

--- Estudo/index.js
+++ Estudo/index.js
@@ -2,3 +2,5 @@ const date = new Date(2025, 10, 12, 20, 45, 6);
 
 // Exibindo a data em formate da minha região
 console.log(date.toLocaleDateString())
+// Exibindo a hora em formate da minha região
+console.log(date.toLocaleTimeString())
