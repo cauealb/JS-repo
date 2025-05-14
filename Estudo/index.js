162b3de8432d5ccd1a5aa93ab40ec6d366d00369
Cauê Alves <cauealvesb4@gmail.com>
2025-05-14 18:28:14 -0300

    Pega o horario, ano e minuto, para aparecer em diferentes posições

--- Estudo/index.js
+++ Estudo/index.js
@@ -6,4 +6,9 @@ const day = aniversario.getDate().toString().padStart(2, "0")
 // Formatando o mês com um zero na frente
 const month = (aniversario.getMonth() + 1).toString().padStart(2, "0");
 
+// Pega o horario, ano e minuto, para aparecer em diferentes posições
+const year = aniversario.getFullYear()
+const hours = aniversario.getHours()
+const minutes = aniversario.getMinutes()
+
 console.log(month)
