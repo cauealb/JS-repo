7cb89acd1a7e00fea89d38dcb308958c6ef6eb45
Cauê Alves <cauealvesb4@gmail.com>
2025-05-13 23:46:23 -0300

    Colocando o nome do mês

--- Estudo/index.js
+++ Estudo/index.js
@@ -6,3 +6,6 @@ console.log(new Date(2024, 2, 20, 23, 39, 0))
 
 // Colocando a data com string
 console.log(new Date("2024-03-20T12:12:12"));
+
+// Colocando o nome do mês
+console.log(new Date("March 20, 2025 13:13:55"))
