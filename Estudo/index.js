bc8727ee791c9ab053cee8a7595f2e60c7a52857
Cauê Alves <cauealvesb4@gmail.com>
2025-05-14 18:26:46 -0300

    Formatando o mês com um zero na frente

--- Estudo/index.js
+++ Estudo/index.js
@@ -3,4 +3,7 @@ const aniversario = new Date("2025-03-01T05:05:05");
 // Formatando o dia com um zero na frente
 const day = aniversario.getDate().toString().padStart(2, "0")
 
-console.log(day)
+// Formatando o mês com um zero na frente
+const month = (aniversario.getMonth() + 1).toString().padStart(2, "0");
+
+console.log(month)
