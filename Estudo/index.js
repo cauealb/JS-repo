ecd80915f07fc0ed41b9448accda1f3f334d309a
Cauê Alves <cauealvesb4@gmail.com>
2025-05-13 22:54:54 -0300

    Exibindo a data

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,12 +1,2 @@
-// Achando um index de um Array
-const arrFruits = ["Banana", "Lemon", "Orange"]
-
-if(arrFruits.includes("Lemon")) {
-    console.log(arrFruits.indexOf("Lemon"))
-} else {
-    console.log("Não exitem dados!")
-}
-
-// Utilizando a função splice
-arrFruits.splice(1, 2)
-console.log(arrFruits)
+// Exibindo a data
+console.log(new Date())
