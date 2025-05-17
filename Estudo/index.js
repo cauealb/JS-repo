e0875e040c8ab380f1a30372474aeafac5344af6
Cauê Alves <cauealvesb4@gmail.com>
2025-05-17 18:50:01 -0300

    Usando uma função estática na classe

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,13 +1,14 @@
 // Criando uma classe sem o construtor
 class User {    
-    showMessage() {
+    // Usando uma função estática na classe
+    static showMessage() {
         console.log("Hello Word Javascript!");
     }
 }
 
 // Tentando usar a classe sem instânciar
-// User.showMessage()
+User.showMessage()
 
 // Intânciando antes de usar a função
-const newUser = new User();
-newUser.showMessage();
+// const newUser = new User();
+// newUser.showMessage();
