8faeeafec5b2df036d1a10cef4fb60a717086b8e
Cauê Alves <cauealvesb4@gmail.com>
2025-05-17 18:47:31 -0300

    Tentando usar a classe sem instânciar

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,4 +1,9 @@
 // Criando uma classe sem o construtor
-class User {
-    
+class User {    
+    showMessage() {
+        console.log("Hello Word Javascript!");
+    }
 }
+
+// Tentando usar a classe sem instânciar
+User.showMessage()
