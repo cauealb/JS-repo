ad07040b9b642a5fd61430710b98e7c76bda7e71
Cauê Alves <cauealvesb4@gmail.com>
2025-05-18 22:34:25 -0300

    Criando uma classe de Anima

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,33 +1,12 @@
-// Fazendo uma classe
+// Criando uma classe de Animal
 class Animal {
-    constructor(name, song) {
+    constructor(name) {
         this.name = name
-        this.song = song
     }
 
-    makeSong() {
-        console.log(this.song);
-    }
-
-    makeName() {
-        console.log(`My name is ${this.name}`);
+    static makeSong() {
+        console.log("Algum som genérico do animal!")
     }
 }
 
-// Fazendo heraça com outra classe
-class Lion extends Animal {
-
-}
-
-const SkarFace = new Lion("ScarFace", "ARRRRRRRRRRR!")
-SkarFace.makeSong()
-SkarFace.makeName()
-
-// Criando uma classe de Dog e fazendo herança com a classe animal
-class Dog extends Animal {
-
-}
-
-const Safira = new Dog("Safira", "AUUUUUUUUUUUUUU");
-Safira.makeName()
-Safira.makeSong();
+Animal.makeSong()
