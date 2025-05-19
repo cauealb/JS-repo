ed44752c8310d38441ab07f6167c6d86300ec129
Cauê Alves <cauealvesb4@gmail.com>
2025-05-18 23:02:05 -0300

    Criando um objeto

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,32 +1,6 @@
-// Criando uma classe de Animal
-class Animal {
-    constructor(name) {
-        this.name = name
-    }
-
-    makeSong() {
-        console.log("Algum som genérico do animal!")
-    }
+// Criando um objeto
+const adress = {
+    city: "São Paulo",
+    street: "Rua XYZ",
+    number: "00"
 }
-
-// Criando uma classe de Leão e de cachorro e fazendo herança com a classe Animal
-class Lion extends Animal {
-    // Sobre escrevendo a função já existênte da classe pai
-    makeSong() {
-        console.log("ARRRRRRRR!")
-    }
-}
-
-// Instânciando a classe de leão e cachorro
-const SkarFace = new Lion("SkarFace")
-SkarFace.makeSong()
-
-class Dog extends Animal {
-    // Sobre escrevendo a função já existênte da classe pai
-    makeSong() {
-        console.log("Woof! Woof! Woof!")
-    }
-}
-
-const Safira = new Dog("Safira")
-Safira.makeSong()
