3e91b0ff6e6676c09fcfbd82cc29b410096f3db1
Cauê Alves <cauealvesb4@gmail.com>
2025-05-18 23:06:33 -0300

    Exibindo o prototype em string

--- Estudo/index.js
+++ Estudo/index.js
@@ -11,3 +11,7 @@ console.log(adress)
 // Exibindo o prototype em Array
 const arr = ["Santos", "Corinthias", "São Paulo", "Palmeiras"]
 console.log(arr)
+
+// Exibindo o prototype em string
+let name = "Cauê"
+console.log(name.__proto__)
