47e56a198f7b828b9f86fdb0f0b81fe598534c45
Cauê Alves <cauealvesb4@gmail.com>
2025-05-20 00:50:51 -0300

    Criando uma classe de erros customizadas

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,22 +1,6 @@
-// Lançando um erro
-let obj = []
-let num = 100
-
-try {
-    
-    if(!obj.length > 0) {
-        throw new RangeError("São existe dados nesse array")
-    }
-    // lançando um erro de período númerico de classe
-    if(num < 99) {
-        throw new RangeError("Número menor que 100");
-    }
-
-} catch (error) {
-    // Verificando a classe que mandou o erro
-    if(error instanceof Error) {
-        console.log("Erro localizado no Error")
-    } else if(error instanceof RangeError) {
-        console.log("Erro localizado no RangeErrors")
+// Criando uma classe de erros customizadas
+class MyError {
+    constructor(message) {
+        this.message = `MyError: ${message}`
     }
 }
