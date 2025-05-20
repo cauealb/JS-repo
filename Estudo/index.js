9206fb20deb5ab99dcfce1cc38ca54c5154789cd
Cauê Alves <cauealvesb4@gmail.com>
2025-05-20 00:40:52 -0300

    Verificando a classe que mandou o erro

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,16 +1,22 @@
 // Lançando um erro
 let obj = []
+let num = 100
 
 try {
     
-    // lançando um erro com as classes de Erros
     if(!obj.length > 0) {
-        throw new Error("São existe dados nesse array")
+        throw new RangeError("São existe dados nesse array")
+    }
+    // lançando um erro de período númerico de classe
+    if(num < 99) {
+        throw new RangeError("Número menor que 100");
     }
 
 } catch (error) {
     // Verificando a classe que mandou o erro
-    if(error instanceof Error) [
+    if(error instanceof Error) {
         console.log("Erro localizado no Error")
-    ]
+    } else if(error instanceof RangeError) {
+        console.log("Erro localizado no RangeErrors")
+    }
 }
