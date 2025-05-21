56745046c74fedf9e70b8c34aa72ace52c7872f9
Cauê Alves <cauealvesb4@gmail.com>
2025-05-21 00:25:22 -0300

    Criando uma função e uma arrow function

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,21 +1,4 @@
-// Criando uma classe de erros customizadas
-class MyError {
-    constructor(message) {
-        this.message = `MyError: ${message}`
-    }
-}
+// Criando uma função e uma arrow function
+function myFunction() {}
 
-try {
-    // Lançando um erro genérico
-    throw new Error("Erro genérico")
-
-    // Colocando o erro customizado
-    throw new MyError("Erro customizado")
-
-} catch (error) {
-    if(error instanceof MyError) {
-        console.log(error.message)
-    } else {
-        console.log("Errro genérico")
-    }
-}
+const myArrowFunction = () => {}
