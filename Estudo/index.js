b9d3497e6540849248703fe3a9c5a8b671f2d3b1
Cauê Alves <cauealvesb4@gmail.com>
2025-05-21 00:34:54 -0300

    usando arguments no Arrow Function

--- Estudo/index.js
+++ Estudo/index.js
@@ -3,5 +3,11 @@ function soma() {
     console.log(arguments)
 }
 
+// soma(1, 2, 3, 4, 5)
 
-soma(1, 2, 3, 4, 5)
+// usando arguments no Arrow Function
+const somaArrow = () => {
+    console.log(arguments)
+}
+
+somaArrow(1, 2, 3, 4)
