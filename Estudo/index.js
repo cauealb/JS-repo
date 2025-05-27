1db283a1f50d57f2600a5cf042bd0f6627ed0b32
Cauê Alves <cauealvesb4@gmail.com>
2025-05-27 02:10:46 -0300

    Criando objeto

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,22 +1,9 @@
-// Criando um objeto
-const user = {
-    firstName: "Geovanna",
+// Criando objeto
+const User = {
+    firstName: "Cauê",
     lastName: "Alves",
-    full_name: "Cauê Alves",
     age: 19,
-    stacks: ["Javascript", "Typescript", "React", "Node"]
+    stacks: ["JS", "TS", "React", "Node"],
+    salary: 2700,
+    cargo: "Desenvolvedor Web Junior"
 }
-
-// Desestruturando
-// const { firstName, lastName, age, stacks, full_name: fullName } = user
-// // Desestruturando array
-// const [primary, secondary, lib, framework] = stacks
-
-// Usando Rest Operator
-const { stacks,...rest } = user
-
-// Aplicando Rest Operator em um array
-const [ primary, ...restArr ] = stacks
-
-
-console.log(rest, restArr)
