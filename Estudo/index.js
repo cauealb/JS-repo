b1654369d93a1fb2b74e595bee9c42e522130c51
Cauê Alves <cauealvesb4@gmail.com>
2025-05-31 14:17:40 -0300

    Criando uma chamada de uma Api fake

--- Estudo/index.js
+++ Estudo/index.js
@@ -2,7 +2,12 @@
 const apiCall = new Promise((resolve, reject) => {
     // Imprimindo o erro ou o sucesso
     // resolve('Sucesso')
-    reject('Errou!')
+    // reject('Errou!')
+
+    // Criando uma chamada de uma Api fake
+    setTimeout(() => {
+        resolve('Sucesso')
+    }, 2000)
 })
 
 console.log(apiCall)
