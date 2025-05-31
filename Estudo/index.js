e913d8ddb267dabddb52ef715b7967164e5b8be9
Cauê Alves <cauealvesb4@gmail.com>
2025-05-31 14:25:07 -0300

    Usando o cath para pegar o valor do erro

--- Estudo/index.js
+++ Estudo/index.js
@@ -6,7 +6,7 @@ const apiCall = new Promise((resolve, reject) => {
 
     // Criando uma chamada de uma Api fake
     setTimeout(() => {
-        resolve('Sucesso')
+        reject('erro na instância')
     }, 2000)
 })
 
@@ -14,4 +14,8 @@ apiCall
     // Usando o then para pegar o valor do resultado
     .then((sucess) => {
         console.log(sucess)
+    })
+    // Usando o cath para pegar o valor do erro
+    .catch((erro) => {
+        console.log(erro)
     })
