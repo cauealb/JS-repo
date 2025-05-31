a6db26e28b649533b0e8f608995cdc588b506cfe
Cauê Alves <cauealvesb4@gmail.com>
2025-05-31 14:10:45 -0300

    Criando uma Promisse

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,29 +1,4 @@
-// Criando objeto
-const User = {
-    firstName: "Cauê",
-    lastName: "Alves",
-    age: 19,
-    stacks: ["JS", "TS", "React", "Node"],
-    salary: 2700,
-    cargo: "Desenvolvedor Web Junior"
-}
-
-// Usando spred oparation + Imutabilidade
-const updateUser = {
-    ...User,
-    lastName: "Alves Barreto",
-    stacks: [...User.stacks, "Next"],
-    salary: 5000,
-    cargo: "Desenvolvedor Full-Stack Javascript + Typescript"
-}
-
-//Desestruturando o objeto
-const {lastName, cargo} = updateUser
-
-// Criando um novo objeto com o antigo
-const newUserUpdate = {
-    ...User,
-    frelancer: "Full Stack Developer React + Next"
-}
-
-console.log(newUserUpdate)
+// Criando uma Promisse
+new Promise((resolve, reject) => {
+    
+})
