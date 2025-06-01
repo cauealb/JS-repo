7240822ffc61c81506cefa9f4c9faf38183e7ba0
Cauê Alves <cauealvesb4@gmail.com>
2025-06-01 09:08:34 -0300

    Usando o startWith

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,11 +1,5 @@
-//  Aprendendo o Includes
-const stacks = ['Node', 'Javascript', 'React', 'Tailwind']
+// Usando o startWith
+const role = 'Desenvolvedor Full Stack Javascript + React'
 
-const includesArray = stacks.includes('react')
-// console.log(includesArray)
-
-// Usando includes com string
-const role = 'Desenvolvedor Full Stack Junior'
-const includesString = role.includes('Junior')
-
-console.log(includesString)
+const start = role.startsWith('Desenvolvedor')
+console.log(start)
