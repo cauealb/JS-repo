68decb88423c46d2752740b5e76537ed836bf625
Cauê Alves <cauealvesb4@gmail.com>
2025-06-01 11:48:56 -0300

    Usando o método find

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,8 +1,13 @@
-// Usando o startWith
-const role = 'Desenvolvedor Full Stack Javascript + React'
+// Usando o método find
+const estoque = [
+    {name: 'iPhone 16 Pro Max', price: 8000, quantity: 2},
+    {name: 'MacBook Pro', price: 20000, quantity: 1},
+    {name: 'iPhne 11', price: 3000, quantity: 1},
+    {name: 'Xaiomi', price: 1500, quantity: 5},
+    {name: 'Máquina de lavar', price: 3000, quantity: 0},
+    {name: 'Pai rico Pai pobre', price: 50, quantity: 10},
+    {name: 'Caderno', price: 15, quantity: 2},
+]
 
-const start = role.startsWith('Desenvolvedor')
-
-// Usando o endsWith
-const end = role.endsWith('React')
-console.log(end)
+const find = estoque.find((item) => item.price > 1500)
+console.log(find)
