75e1aeb98bc8b3045ac9ea39f3146034eacdd214
Cauê Alves <cauealvesb4@gmail.com>
2025-06-01 08:57:22 -0300

    Aprendendo o Includes

--- Estudo/index.js
+++ Estudo/index.js
@@ -1,30 +1,5 @@
-// Criando uma Promisse
-const apiCall = new Promise((resolve, reject) => {
-    // Criando uma chamada de uma Api fake
-    setTimeout(() => {
-        reject('Erro')
-    }, 2000)
-})
+//  Aprendendo o Includes
+const stacks = ['Node', 'Javascript', 'React', 'Tailwind']
 
-// apiCall
-//     // Usando o then para pegar o valor do resultado
-//     .then((sucess) => {
-//         console.log(sucess)
-//     })
-//     // Usando o cath para pegar o valor do erro
-//     .catch((erro) => {
-//         console.log(erro)
-//     })
-
-// Usando o Async/Await
-async function run() {
-    // Usando o try para pegar o erro na promisse
-   try{
-    const reposta = await apiCall
-    console.log(reposta)
-   } catch(error) {
-        console.log(error)
-   } 
-}
-
-run()
+const includesArray = stacks.includes('react')
+console.log(includesArray)
